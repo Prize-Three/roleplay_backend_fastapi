3f895b6387d1856c6b8aa3bc57baac150039da65
BoyoungH <hyong0903@naver.com>
2024-07-27 14:10:46 +0900

    Feat: Mysql 연결 위한 database.py 추가

---
+++ app/database/database.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# .env 파일 로드
+load_dotenv()
+
+# 환경변수에서 DATABASE_URL 가져오기
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
