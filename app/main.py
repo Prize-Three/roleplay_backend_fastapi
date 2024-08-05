f508d6e5bce1367e51b34bc106d620f144af7df5
BoyoungH <hyong0903@naver.com>
2024-08-06 00:23:19 +0900

    Feat: voicelist.py 라우터 추가

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import mysql.models as models
 
 from mysql.database import async_engine
-from api import server, resultreport
+from api import server, resultreport, voicelist
 
 # models.Base.metadata.create_all(bind=engine)
 # models.Base.metadata.create_all(bind=async_engine.sync_engine)
@@ -28,6 +28,7 @@ app.add_middleware(
 
 app.include_router(server.router, prefix="/server")
 app.include_router(resultreport.router, prefix="/report")
+app.include_router(voicelist.router, prefix="/voice")
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
