47d927d9f960f66012bae0998afa641d8b09bcbb
BoyoungH <hyong0903@naver.com>
2024-08-11 23:43:11 +0900

    Feat: voice_id로 history 조회 함수 추가

--- app/crud/history.py
+++ app/crud/history.py
@@ -1,15 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from mysql.models import History, User, Voice
-from datetime import datetime
+from datetime import time
 
 # History 레코드 생성 함수
-async def create_history(db: AsyncSession, user_id: int, voice_id: int, situation: str, duration: datetime, my_role: str, ai_role: str):
+async def create_history(db: AsyncSession, user_id: int, voice_id: int, situation: str, start_time: time, my_role: str, ai_role: str):
     db_history = History(
         user_id=user_id,
         voice_id=voice_id,
         situation=situation,
-        duration=duration,
+        start_time=start_time,
         my_role=my_role,
         ai_role=ai_role
     )
@@ -22,6 +22,10 @@ async def create_history(db: AsyncSession, user_id: int, voice_id: int, situatio
 async def get_histories_by_user_id(db: AsyncSession, user_id: int):
     result = await db.execute(select(History).where(History.user_id == user_id))
     return result.scalars().all()
+
+async def get_history_by_voice_id(db: AsyncSession, voice_id: int):
+    result = await db.execute(select(History).where(History.voice_id == voice_id))
+    return result.scalars().first()
 
 # 특정 History 레코드 조회 함수
 async def get_history_by_id(db: AsyncSession, history_id: int):
