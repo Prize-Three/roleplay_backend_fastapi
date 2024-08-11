939f45c0329281fadd35a286ea6bf3432f57be34
BoyoungH <hyong0903@naver.com>
2024-08-11 23:42:11 +0900

    Feat: dialog crud 코드 작성

---
+++ app/crud/dialog.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from mysql.models import Dialog
+
+async def create_dialog(db: AsyncSession, history_id: int, speaker: str, message_content: str):
+    db_dialog = Dialog(
+        history_id=history_id,
+        speaker=speaker,
+        message=message_content
+    )
+    db.add(db_dialog)
+    await db.commit()
+    await db.refresh(db_dialog)
+    return db_dialog
+
+async def get_dialogs_by_history_id(db: AsyncSession, history_id: int):
+    result = await db.execute(select(Dialog).where(Dialog.history_id == history_id))
+    return result.scalars().all()
