5ad330e9138987630c8361cf11056b949cda6f74
BoyoungH <hyong0903@naver.com>
2024-08-17 12:59:02 +0900

    Feat: 비동기 처리를 위한 create_used_sentence crud 구현

--- app/crud/usedsentence.py
+++ app/crud/usedsentence.py
@@ -14,3 +14,21 @@ async def get_used_sentences_by_report_id(db: AsyncSession, report_id: int, deve
 async def get_used_sentence_by_id(db: AsyncSession, used_sentence_id: int) -> UsedSentence:
     result = await db.execute(select(UsedSentence).where(UsedSentence.id == used_sentence_id))
     return result.scalars().first()
+
+async def create_used_sentence(
+    db: AsyncSession, 
+    result_report_id: int, 
+    development_type: bool, 
+    dialog_content: str, 
+    comment: str
+) -> UsedSentence:
+    new_sentence = UsedSentence(
+        result_report_id=result_report_id,
+        development_type=development_type,
+        dialog_content=dialog_content,
+        comment=comment
+    )
+    db.add(new_sentence)
+    await db.commit()
+    await db.refresh(new_sentence)
+    return new_sentence
